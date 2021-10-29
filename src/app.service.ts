dc57d288f15a3b0f822cb3436c413d0e6125dce8
Daniel Ileyko <rlejko13@gmail.com>
2021-10-29 17:03:46 +0300

    getBranchByName is working

--- src/app.service.ts
+++ src/app.service.ts
@@ -39,24 +39,24 @@ export class AppService {
       });
   }
 
-  // async getBranchByName(name: string) {
-  //   const url = `https://api.github.com/repos/nodejs/node/commits?per_page=25&sha=${name}`;
+  async getBranchByName(name: string) {
+    const url = `https://api.github.com/repos/nodejs/node/commits?per_page=25&sha=${name}`;
 
-  //   await axios.get(url).then((response: any) => {
-  //     console.log(
-  //       response.data
-  //         .sort((a, b) =>
-  //           a.commit.committer.date > b.commit.committer.date ? -1 : 1,
-  //         )
-  //         .map((commit) => ({
-  //           author: commit.commit.author.name,
-  //           message: JSON.stringify(commit.commit.message).split('\\')[0],
-  //           sha: commit.sha,
-  //           date: commit.commit.committer.date,
-  //         })),
-  //     );
-  //   });
-  // }
+    await axios.get(url).then((response: any) => {
+      console.log(
+        response.data
+          .sort((a, b) =>
+            a.commit.committer.date > b.commit.committer.date ? -1 : 1,
+          )
+          .map((commit) => ({
+            author: commit.commit.author.name,
+            message: JSON.stringify(commit.commit.message).split('\\')[0],
+            sha: commit.sha,
+            date: commit.commit.committer.date,
+          })),
+      );
+    });
+  }
 
   // async getBranchWithFilter(name: string, filter: string) {
   //   const url = `https://api.github.com/repos/nodejs/node/commits?per_page=25&sha=${name}`;
